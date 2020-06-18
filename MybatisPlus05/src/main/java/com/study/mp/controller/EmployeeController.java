3545f3cc6cdb8110a23fc4a7433de9beb7314d6f
hql0512 <huoquanlong0512>
2020-06-18 18:25:32 +0800

    3th commit

---
+++ MybatisPlus05/src/main/java/com/study/mp/controller/EmployeeController.java
@@ -0,0 +1,21 @@
+package com.study.mp.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author hql
+ * @since 2020-06-14
+ */
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+}
+
---
+++ MybatisPlus05/src/main/java/com/study/mp/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.study.mp.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.study.mp.beans.User;
+
+public interface UserMapper extends BaseMapper<User>{
+
+}
